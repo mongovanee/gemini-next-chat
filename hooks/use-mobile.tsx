e52b893e534042de9aacebc46dd96f205df6a3d6
mongovanee <58361386+mongovanee@users.noreply.github.com>
2026-05-24 20:20:58 +0800

    修复ios15低版本白屏

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -1,3 +1,20 @@
+// import { useState, useEffect } from 'react'
+
+// export function useIsMobile(mobileBreakpoint = 768) {
+//   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined)
+
+//   useEffect(() => {
+//     const mql = window.matchMedia(`(max-width: ${mobileBreakpoint - 1}px)`)
+//     const onChange = () => {
+//       setIsMobile(window.innerWidth < mobileBreakpoint)
+//     }
+//     mql.addEventListener('change', onChange)
+//     setIsMobile(window.innerWidth < mobileBreakpoint)
+//     return () => mql.removeEventListener('change', onChange)
+//   }, [mobileBreakpoint])
+
+//   return !!isMobile
+// }
 import { useState, useEffect } from 'react'
 
 export function useIsMobile(mobileBreakpoint = 768) {
@@ -8,9 +25,19 @@ export function useIsMobile(mobileBreakpoint = 768) {
     const onChange = () => {
       setIsMobile(window.innerWidth < mobileBreakpoint)
     }
-    mql.addEventListener('change', onChange)
+    if (mql.addEventListener) {
+      mql.addEventListener('change', onChange)
+    } else {
+      mql.addListener(onChange)
+    }
     setIsMobile(window.innerWidth < mobileBreakpoint)
-    return () => mql.removeEventListener('change', onChange)
+    return () => {
+      if (mql.removeEventListener) {
+        mql.removeEventListener('change', onChange)
+      } else {
+        mql.removeListener(onChange)
+      }
+    }
   }, [mobileBreakpoint])
 
   return !!isMobile
